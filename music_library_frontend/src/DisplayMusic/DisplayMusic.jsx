8bfa5d111718ef5b95baa0e9e99ac8b2306fb926
Charley <charley1135@gmail.com>
2022-12-06 16:13:31 -0800

    feat: Like button complete.

--- music_library_frontend/src/DisplayMusic/DisplayMusic.jsx
+++ music_library_frontend/src/DisplayMusic/DisplayMusic.jsx
@@ -2,22 +2,45 @@ import React, { useState, useEffect } from 'react';
 import './DisplayMusic.css'
 import '../EditModal/EditModal'
 import Modal from '../EditModal/EditModal';
+import axios from 'axios';
 
 
 
 
 const DisplayMusic = ({song, getAllSongs}) => {
 
-    
-   
-    
-    const [likeButton, setLikeButton] = useState(song.likes) // When I click the like button, it doesn't show 1, but shows 2 and after. Need to make this an API request to 'put' a change to increment.'
-    const [showModal, setShowModal] = useState(false)
+
+const [showModal, setShowModal] = useState(false)
+// TODO: Combine handlePut and handleClick
+
+    const handlePut = async (s) => {
+        
+        try {
+            await axios.put(`http://127.0.0.1:8000/music/${song.id}/`,s)
+            .then(response=>{console.log(response); 
+                })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleClick = async () => {
+
+
+        let likesButton = {
+
+        title: song.title,
+        artist: song.artist,
+        album: song.album,
+        release_date: song.release_date,
+        genre: song.genre,
+        likes: song.likes + 1,
+        };
+        await handlePut(likesButton).then(response => getAllSongs());
+
+    }
 
     
-
-
-   
 
 
     return ( 
@@ -28,7 +51,7 @@ const DisplayMusic = ({song, getAllSongs}) => {
                 <td>{song.release_date}</td>
                 <td>{song.genre}</td>
                 <td>{song.likes}</td>
-                <td><button type='submit' onClick={()=> setLikeButton(song.likes ++)}  style={{ cursor: "pointer" }}>Like</button></td>
+                <td><button onClick={()=> handleClick()}  style={{ cursor: "pointer" }}>Like</button></td>
                 <td><button onClick={()=> setShowModal(true)}>Edit</button></td>
                 <Modal open={showModal} song = {song} getAllSongs={getAllSongs} onClose={()=>setShowModal(false)}/>
                 
